0f2d8aeb6b8ed64da61bdd328d20d5e9521618aa
Local Rails IDE <kevin.murphy17@yahoo.com>
2018-02-24 16:07:07 -0500

    footer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def easypost
   end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#easypost'
+  get 'about', to: 'welcome#about'
   get 'ordertee', to: 'pages#order'
-  get 'trackertee', to: 'pages#tracker' 
+  get 'trackertee', to: 'pages#tracker'
 end
