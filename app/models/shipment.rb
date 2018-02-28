7c90b51cc5706f7f5e60373ebf6dd9b7bf7b4216
Local Rails IDE <kevin.murphy17@yahoo.com>
2018-02-27 20:13:57 -0500

    shipments

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -6,20 +6,35 @@ class Shipment
   end
 
   def name
+    @name = ""
   end
 
   def company
+    @company = ""
   end
 
   def address
+    @address = ""
   end
 
   def city
+    @city = ""
   end
 
   def state
+    @state = ""
   end
 
   def zip
+    @zip = ""
+  end
+
+  def initialize(shipment_params)
+    @name = shipment_params[:name]
+    @company = shipment_params[:company]
+    @address = shipment_params[:address]
+    @city = shipment_params[:city]
+    @state = shipment_params[:state]
+    @zip = shipment_params[:zip]
   end
 end
