65621c15d4c14562a10ff2824247c7b9b7b47eb3
Local Rails IDE <kevin.murphy17@yahoo.com>
2018-03-12 22:21:58 -0400

    shipments

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -10,8 +10,8 @@ class Shipment
   end
 
   attr_accessor :name, :company, :address, :city, :state, :zip
-
-  validates_each :name, :company, :address, :city, :state, :zip, allow_blank: false
+  validates :address, allow_blank: false
+  #validates_each :name, :company, :address, :city, :state, :zip, allow_blank: false
 
   def initialize
 
